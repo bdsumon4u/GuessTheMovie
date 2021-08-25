fa0a42225aa57c26e8708fc53c14399cd5bc9890
Sumon Ahmed <bdsumon4u@gmail.com>
2021-08-25 14:08:45 +0600

    Read & Print MovieList From File

--- src/tech/hotash/java2/Main.java
+++ src/tech/hotash/java2/Main.java
@@ -3,6 +3,6 @@ package tech.hotash.java2;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        MovieBox.list();
     }
 }
---
+++ src/tech/hotash/java2/MovieBox.java
@@ -0,0 +1,22 @@
+package tech.hotash.java2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class MovieBox {
+    private static final File movies = new File("src/movies.txt");
+
+    public static void list() {
+        try {
+            Scanner sc = new Scanner(movies);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+            sc.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}

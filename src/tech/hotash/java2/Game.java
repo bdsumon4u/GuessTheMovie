322e632797aa8e496dc1ae318d6279582c1bd2a3
Sumon Ahmed <bdsumon4u@gmail.com>
2021-08-25 20:07:34 +0600

    Reveal CHAR, Game Over, Game Win

--- src/tech/hotash/java2/Game.java
+++ src/tech/hotash/java2/Game.java
@@ -18,13 +18,21 @@ public class Game {
     public void play() {
         char charNow;
         Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < movie.length(); i++) {
+        for (int i = 0; this.score != movie.length(); i++) {
             print_();
             wrongs();
+
+            System.out.println("--" + this.score + "--");
+            if (this.wrong_count == 10) {
+                break;
+            }
+
             System.out.print("Guess a letter: ");
             charNow = sc.next().charAt(0);
             this.search_char(charNow);
         }
+        sc.close();
+        result();
     }
 
     private char[] to_(String movie) {
@@ -56,7 +64,10 @@ public class Game {
         boolean found = false;
         for (int i = 0; i < this.movie.length(); i++) {
             if (this.movie.charAt(i) == ch) {
-                System.out.print(ch);
+                if (this.guess[i] != ch) {
+                    this.guess[i] = ch;
+                    this.score++;
+                }
                 found = true;
             }
         }
@@ -64,6 +75,15 @@ public class Game {
 
         if (!found) {
             this.wrong[this.wrong_count++] = ch;
+        }
+    }
+
+    private void result() {
+        if (this.score == this.movie.length()) {
+            System.out.println("You win!");
+            System.out.println("You have guessed '" + this.movie + "' correctly.");
+        } else {
+            System.out.println("You've Lost The Game.");
         }
     }
 }
